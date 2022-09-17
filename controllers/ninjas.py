5b1bdc7ccc93112d8c1f2297a059c8078dad867b
Andr3w1992 <94875932+Andr3w1992@users.noreply.github.com>
2022-09-17 02:29:12 -0500

    first commit

---
+++ controllers/ninjas.py
@@ -0,0 +1,14 @@
+from crypt import methods
+from flask import render_template, redirect
+from flask_app import app
+from flask_app import dojo, ninja
+
+@app.route('/ninjas')
+def ninjas():
+    return render_template('ninja.html',dojos= dojo.Dojo.get_all())
+
+
+@app.route('/create/ninja', methods=['POST'])
+def create_ninja():
+    ninja.Ninja.save(request.form)
+    return redirect('/')
